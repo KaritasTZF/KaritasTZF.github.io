11f0476d73628aaf1b94613ae862e7f9672dd21e
Karitas <ktz1@hi.is>
2025-09-12 15:43:08 +0000

    froskur: variable lanes

--- verkefni1/froskur.js
+++ verkefni1/froskur.js
@@ -4,24 +4,15 @@ var gl;
 var maxX = 1.0; //plusminus
 var maxY = 1.0; //plusminus
 
-// Upphafsstaða frosksins
-var verticesFrog = new Float32Array([-0.05,-0.05, 0.05,-0.05, 0,0.05]);
-
-// akgreinar hnit
-var verticesLanes = new Float32Array([
-    1.0,0.6, -1.0,0.6, 
-    1.0,0.2, -1.0,0.2, 
-    1.0,-0.2, -1.0,-0.2, 
-    1.0,-0.6, -1.0,-0.6 
-]);
-
-var verticesCars; // Hnit bíla
-var carFj = 4; // Fjöldi bíla
-var carX = new Float32Array(carFj); // changes
-var carY = new Float32Array(carFj); // middle of lane
+var carFj = 3; // Fjöldi bíla
+var carX = new Float32Array(carFj); // x-hnit bíla, changes
+var carY = new Float32Array(carFj); // y-hnit bíla, middle of lane
 var carSpeed = new Float32Array(carFj);
 var carHalfHeight = (0.4 -0.1)/2;
-var carHalfLength = carHalfHeight*1.8;
+var carHalfLength = carHalfHeight*1.0;
+
+var laneFj = 3; // Fjöldi akgreina
+var laneSize = 2.0/(laneFj+2.0); // stærð akgreina á canvas
 
 // buffers
 var bufferLanes;
@@ -44,7 +35,10 @@ window.onload = function init()
     //
     // Initialize starting values of variables
     //
-    generateCars();
+    // vertices
+    var verticesFrog = new Float32Array([-0.05,-0.05, 0.05,-0.05, 0,0.05]);
+    var verticesCars = generateCars();
+    var verticesLanes = generateLanes();
 
     //
     // Load shaders
@@ -70,15 +64,27 @@ window.onload = function init()
     render();
 }
 
+// Generates lane vertices for TRIANGLE_STRIP using laneFj
+function generateLanes() {
+    var helpHnit = new Array(laneFj+1);
+    var y = 0;
+    for (var i = 0; i <= laneFj; ++i ) {
+        y = maxY-laneSize*(i+1);
+        helpHnit[i] = [-maxX,y, +maxX, y];
+    }
+    return new Float32Array(helpHnit.flat(Infinity));
+}
+
 // Sets variables for cars, random start etc using carFj
+// returns vertices
 function generateCars() {
     var helpHnit = new Array(carFj*4);
     var lane;
     for (var i = 0; i < carFj; ++i ) {
-        lane = i % 3;
+        lane = i % laneFj;
         carSpeed[i] = 0.005*(lane+1.0);
         carX[i] = Math.random()*2.0-1.0;
-        carY[i] = (lane-1.0)*0.4;
+        carY[i] = (lane-((laneFj-1.0)/2.0))*laneSize;
         helpHnit[i] = [
             vec2( - carHalfLength, - carHalfHeight),
             vec2( - carHalfLength, + carHalfHeight),
@@ -86,7 +92,7 @@ function generateCars() {
             vec2( + carHalfLength, - carHalfHeight),
         ];
     }
-    verticesCars = flatten(helpHnit.flat(Infinity));
+    return new Float32Array(helpHnit.flat(Infinity));
 }
 
 function render() {
@@ -95,9 +101,9 @@ function render() {
     // Setjum litinn sem hvítann og teiknum akgreinanna
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferLanes);
     gl.vertexAttribPointer( locPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.uniform4fv( locColor, flatten(vec4(1.0, 1.0, 1.0, 1.0)) );
+    gl.uniform4fv( locColor, flatten(vec4(0.6, 0.6, 0.6, 1.0)) );
     gl.uniform2fv( locOffset, flatten(vec2(0.0,0.0)) );
-    gl.drawArrays(gl.LINES, 0, 4*2);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, (laneFj+1)*2);
 
     // Setjum litinn sem fjólubláann og teiknum bílanna
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferCars);
